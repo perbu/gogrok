0c92191efc7abea7be4909d577256f2ee58ea425
Per Buer <per.buer@gmail.com>
2024-03-06 20:14:26 +0100

    license.

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
-package gogrok
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	err := run(ctx, os.Stdout, os.Environ(), os.Args)
+	if err != nil {
+		fmt.Println("run() returned an error: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, output io.Writer, env, args []string) error {
+	fmt.Println("run() called")
+	return nil
+}
