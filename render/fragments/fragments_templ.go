23feb23b3e553ee8b9d62cfdb0eaf1aa2917c9fd
Per Buer <per.buer@gmail.com>
2024-03-13 20:01:12 +0100

    misc

--- render/fragments/fragments_templ.go
+++ render/fragments/fragments_templ.go
@@ -405,7 +405,7 @@ func Package(pkg *analytics.Package) templ.Component {
 			templ_7745c5c3_Var21 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"file\"><h3>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"package\"><h3>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
